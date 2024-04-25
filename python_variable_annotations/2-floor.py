57c320b119ccbeedbadd07eac681d79e67c92655
ChaimaBSlima <chaimabslima@outlook.fr>
2024-04-25 13:18:58 +0100

    project 1 the right repositry

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' 
+chaima ben slima 
+'''
+
+
+def floor(n: float) -> int:
+    ''' Return largest int value less than or equal to n. '''
+    return int(n) if n >= 0 else int(n) - 1
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' 
+chaima ben slima 
+'''
+
+def to_str(n: float) -> str:
+    ''' Return n as a string. '''
+    return str(n)
