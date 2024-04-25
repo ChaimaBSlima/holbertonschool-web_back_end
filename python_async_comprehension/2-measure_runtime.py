58c26cc69f51db9e5bd14f586d98f9b1188ff223
ChaimaBSlima <chaimabslima@outlook.fr>
2024-04-25 13:33:59 +0100

    Project 3 right repositry

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Chaima Ben Slima
+"""
+
+import asyncio
+import time
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measure runtime"""
+    start = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+
+    return time.time() - start
