08b2ce7c84ac3ec26ae4f96c16f7b19b1c2db544
ChaimaBSlima <chaimabslima@outlook.fr>
2024-05-09 11:29:15 +0100

    make it excutable

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const array = []
   for (const idx of array) {
-    value = array[idx];
-    array[idx] = appendString + value;
+    array.push(appendString + idx);
   }
 
   return array;
