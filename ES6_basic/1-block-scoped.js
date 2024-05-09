b56684ab93677e5cbfc98c12090a2638442085df
ChaimaBSlima <chaimabslima@outlook.fr>
2024-05-09 11:27:24 +0100

    task add a comment

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,9 @@
-/* Chaima Ben Slima */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-
+    /* No code */
   }
 
   return [task, task2];
